ffbd13e5ea046494f970ba8611bb04ad7b61c8a1
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-12-10 18:49:48 -0500

    Better comment for write your code

--- metaprogramming_cpp/unroll_for.cpp
+++ metaprogramming_cpp/unroll_for.cpp
@@ -3,12 +3,12 @@ using namespace std;
 template<int start, int end, int inc>
 struct _for {
 	static void run() {
-		// Write code here!!!
-		_for<start + inc, end, inc >::run;
+		/* Write your code here!!! */
+		_for<start + inc, end, inc >::run();
 	}
 };
 
-template<int end, int inc> 
+template<int end, int inc>
 struct _for<end, end, inc > {
 	static void run() {
 		return;
