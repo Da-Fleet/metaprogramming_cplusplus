e53b14bfc7e458b985f9147c758aa9b27c1f33bb
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-12-10 23:01:32 -0500

    Implement Point class with constexpr

--- metaprogramming_cpp/constexpr.cpp
+++ metaprogramming_cpp/constexpr.cpp
@@ -3,3 +3,9 @@ using namespace std;
 constexpr int Fibonacci(int n) {
 	return n == 0 || n == 1 ? 1 : Fibonacci(n - 1) + Fibonacci(n - 2);
 }
+
+class Point {
+public:
+	double X, Y;
+	constexpr Point(double x, double y) : X(x), Y(y) {}
+};
