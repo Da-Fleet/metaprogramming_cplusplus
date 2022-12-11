6f5eb1a5bb9711d6eec069bc892b8c5018fce450
IronBeardX <alejandroyeroval@gmail.com>
2022-12-10 20:24:35 -0500

    Fibonacci and For Unrolling

--- metaprogramming_cpp/main.cpp
+++ metaprogramming_cpp/main.cpp
@@ -2,63 +2,10 @@
 //
 
 #include <iostream>
+#include "unroll_for.cpp"
+#include "fibonacci.cpp"
 
 using namespace std;
-
-
-#pragma region Fibonacci
-
-template<int n>
-struct Fibonnacci
-{
-    static const int value = Fibonnacci<n - 1>::value + Fibonnacci<n - 2>::value;
-};
-
-template<>
-struct Fibonnacci<0>
-{
-    static const int value = 0;
-};
-
-template<>
-struct Fibonnacci<1>
-{
-    static const int value = 1;
-};
-
-#pragma endregion
-
-#pragma region ForLoop
-
-//static unroller
-template <typename Action, int Begin, int End, int Step = 1>
-struct UnrollerS {
-    static void step() {
-        Action::action(Begin);
-        UnrollerS<Action, Begin+Step, End, Step>::step();
-    }
-};
-//end of static unroller
-template <typename Action, int End, int Step>
-struct UnrollerS<Action, End, End, Step> {
-    static void step() { }
-};
-
-//dynamic unroller
-template <typename Action, int Begin, int End, int Step = 1>
-struct UnrollerD {
-    static void step(Action& a) {
-        a.action(Begin);
-        UnrollerD<Action, Begin+Step, End>::step(a);
-    }
-};
-//end of dynamic unroller
-template <typename Action, int End, int Step>
-struct UnrollerD<Action, End, End, Step> {
-    static void step(Action& a) { }
-};
-
-#pragma endregion
 
 struct Printer{
     static void action(int i) {
